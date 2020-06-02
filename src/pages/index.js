7cfac48268805c1b11ecd5ef0ee8aafbbef9af9f
mykol225 <mworthing5@gmail.com>
2020-06-02 06:52:43 -0600

    placeholder state fix
    refactored value simple to remove container
    css: show offset line (box-shadow) when user hovers input as placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import '../components/styles.scss'
 import Label from '../components/label'
 import SimpleWIcon from '../components/SimpleWIcon'
 import ValueMultiple from '../components/ValueMultiple'
+import ValueSimple from '../components/ValueSimple[old]'
 import ICONS from '../components/ICONS' //passing this info down into SimpleIcon then Icon component to render the right icon path.
 
 const IndexPage = () => (
   <div>
-    {/* <ValueSimple placeholder="empty" /> */}
+    <input type="text" className="ValueSimple" placeholder="empty" />
+    <ValueSimple placeholder="empty" />
     <Label labelName="test string" />
     <SimpleWIcon icon={ICONS.LINKCONNECTED} placeholder="empty" />{' '}
     {/* if icon=null then it will remove the icon completely */}
@@ -16,3 +18,7 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+//removed input from it's container and component.
+//this allowed me to style it correctly
+//now I have to put it in Value mulitple component and fix the css.
